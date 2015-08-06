645729ab401a45667c1586b4813ac3803d578b8f
Stanislav <ryabokonsm@mail.ru>
2015-08-06 18:53:44 +0300

    Created a header for RadarView

---
+++ app/src/main/java/com/main/android/wi_fi_radar/RadarView.java
@@ -0,0 +1,22 @@
+package com.main.android.wi_fi_radar;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RadarView extends View {
+    public RadarView(Context context) {
+        super(context);
+    }
+
+    public RadarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
